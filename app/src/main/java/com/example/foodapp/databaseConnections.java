132419f698695231b7faba3de062f7b6cd438a66
Bcaguana <caguana.brandon@gmail.com>
2019-10-09 20:20:58 -0400

    Adds database keys

---
+++ app/src/main/java/com/example/foodapp/databaseConnections.java
@@ -0,0 +1,8 @@
+package com.example.foodapp;
+
+public class databaseConnections {
+    static final String Database_Url= "localhost:";
+    static final String Database_Name= "";
+    static final String Username= "root";
+    static final String Password= "railgun3";
+}
