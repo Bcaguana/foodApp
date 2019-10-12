cf223370b3f0dd1549a6478e1a109ea2d42530e1
Bcaguana <caguana.brandon@gmail.com>
2019-10-11 21:23:32 -0400

    -Added Database Connection Code -Added ListLayout code and button

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.getInventoryButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                GetData retrieveData = new GetData();
+                retrieveData.execute("");
+            }
+        });
+    }
+    private class GetData extends AsyncTask<String, String, String>{
+
+        static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+        static final String DB_URL = "jdbc:mysql://"+
+                databaseConnections.Database_Url+ "/" +
+                databaseConnections.Database_Name;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Connection conn;
+            Statement stmt;
+            try {
+                Class.forName(JDBC_DRIVER);
+                conn = DriverManager.getConnection(DB_URL, databaseConnections.Username, databaseConnections.Password);
+
+                stmt = conn.createStatement();
+                String sqlCommand = "Select * From foodInventory";
+                ResultSet r = stmt.executeQuery(sqlCommand);
+
+                while (r.next()){
+                    String foodNames = r.getString("foodNames");
+                    String expiirationDates = r.getString("expirationDates");
+                };
+                r.close();
+                stmt.close();
+                conn.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/foodapp/databaseConnections.java
+++ app/src/main/java/com/example/foodapp/databaseConnections.java
@@ -1,8 +1,9 @@
 package com.example.foodapp;
 
+//Strings needed to access and alter database
 public class databaseConnections {
-    static final String Database_Url= "localhost:";
-    static final String Database_Name= "";
+    static final String Database_Url= "localhost:3306";
+    static final String Database_Name= "myfooddata";
     static final String Username= "root";
     static final String Password= "railgun3";
 }
