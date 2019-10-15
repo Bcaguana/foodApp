d83aa1b95998e1a8487aa918b33bb9cb8eb3bfa7
Bcaguana <caguana.brandon@gmail.com>
2019-10-14 23:20:41 -0400

    -Successfully printed list of content from sql database to app -Changed databaseConnections String variables -Altered list_of_food_layout.xml to have text views anchor to margins and align with eachother

--- app/src/main/java/com/example/foodapp/databaseConnections.java
+++ app/src/main/java/com/example/foodapp/databaseConnections.java
@@ -2,8 +2,8 @@ package com.example.foodapp;
 
 //Strings needed to access and alter database
 public class databaseConnections {
-    static final String Database_Url= "localhost:3306";
+    static final String Database_Url= "192.168.1.216";
     static final String Database_Name= "myfooddata";
-    static final String Username= "root";
+    static final String Username= "branditnew";
     static final String Password= "railgun3";
 }
